87fc4ca6905bf8c3e8637467324f82ac0af5ff7a
jharteaga <jh.arteaga09@gmail.com>
2020-05-29 23:02:26 -0700

    Add NavBar Component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import logo from '../img/logo.png';
 
-function NavBar(props) {
-  return <div></div>;
-}
+const NavBar = (props) => (
+  <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>
+        <img
+          alt=""
+          src={logo}
+          width="33"
+          height="33"
+          className="d-inline-block align-top logo"
+        />{' '}
+        To-Do List
+      </Navbar.Brand>
+    </Navbar>
+  </>
+);
 
 export default NavBar;
