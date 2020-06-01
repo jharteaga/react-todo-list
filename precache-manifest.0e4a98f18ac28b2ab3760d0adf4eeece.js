b4b33215419cab29ba507b8e56952cbf7ed9c14b
jharteaga <jh.arteaga09@gmail.com>
2020-05-31 18:34:31 -0700

    Updates

---
+++ precache-manifest.0e4a98f18ac28b2ab3760d0adf4eeece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0719fad2311e285f14d4363ce1d9722c",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "76ca04b3f599650d5681",
+    "url": "/react-todo-list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "13dd3e307c1b4c772822",
+    "url": "/react-todo-list/static/css/main.04fdd7b3.chunk.css"
+  },
+  {
+    "revision": "76ca04b3f599650d5681",
+    "url": "/react-todo-list/static/js/2.fdf17cc0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-todo-list/static/js/2.fdf17cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13dd3e307c1b4c772822",
+    "url": "/react-todo-list/static/js/main.6c15fb88.chunk.js"
+  },
+  {
+    "revision": "9b636a31f878b4f150d7",
+    "url": "/react-todo-list/static/js/runtime-main.bf14a083.js"
+  }
+]);
